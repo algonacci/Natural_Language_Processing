5c4a7f0e4424fb19604fa09f241b50d765d71022
Eric Julianto <86970816+algonacci@users.noreply.github.com>
2022-11-09 09:34:01 +0700

    learnt recording sound with python

---
+++ Audio/01 - Audio Recorder/record.py
@@ -0,0 +1,31 @@
+import pyaudio
+import wave
+
+audio = pyaudio.PyAudio()
+stream = audio.open(format=pyaudio.paInt16,
+                    channels=1,
+                    rate=44100,
+                    input=True,
+                    frames_per_buffer=1024)
+frames = []
+
+print("====== Start recording ======")
+
+try:
+    while True:
+        data = stream.read(1024)
+        frames.append(data)
+except KeyboardInterrupt:
+    print("====== Stop recording ======")
+    pass
+
+stream.stop_stream()
+stream.close()
+audio.terminate()
+
+sound_file = wave.open("record.wav", "w")
+sound_file.setnchannels(1)
+sound_file.setsampwidth(audio.get_sample_size(pyaudio.paInt16))
+sound_file.setframerate(44100)
+sound_file.writeframes(b"".join(frames))
+sound_file.close()
